71279c2f9fae33eaf7a4450f1d32553215988fd9
qq6264214 <371998331@qq.com>
2019-09-25 13:49:39 +0800

    Add files via upload

--- sports_front/src/components/HelloWorld.vue
+++ sports_front/src/components/HelloWorld.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="hello">
+   <el-col
+    :span="20"
+    :offset="2"
+    style="height:100%"
+  >
     <el-card class="box-card">
     <div slot="header" class="clearfix">
       <span class="condition-name" >比赛时间:</span>
@@ -16,7 +20,16 @@
         <el-input v-model="num" placeholder="请输入0-1之间的数字" style="width:150px;float:left"></el-input>
         <!-- <span class="condition-name" >冲突阈值:</span>
         <el-input v-model="conflictVal" placeholder="请输入0-1之间的数字" style="width:150px;float:left"></el-input> -->
-        <el-button type="primary" icon="el-icon-search" style="float:left" @click="initPredictions">搜索</el-button>      
+        <el-button type="primary" icon="el-icon-search" style="float:left" @click="initPredictions">搜索</el-button>     
+        <el-alert
+          :title="anaFinished?'分析成功':'正在分析中'"
+          :type="anaFinished?'success':'error'"
+          effect="dark"
+          :style="{display:getFinished?'':'none'}"
+          style="width: 200px;float: left;margin-left: 20px;"
+          :closable="false"
+          >
+        </el-alert>
           <el-upload
           class="upload-demo"
           :action="upload1Datapath" 
@@ -47,7 +60,8 @@
           :data="tableData"
           style="width: 100%"
           height="750"         
-          :cell-class-name="setCellClass">
+          :row-class-name="tableRowClassName"
+          >
           <el-table-column
             prop="type"
             label="类型"
@@ -109,11 +123,34 @@
                 :type="scope.row.isWorth === true ? 'danger' : ''"
                 disable-transitions>{{scope.row.isWorth === true?'是':'否'}}</el-tag>
             </template>
+          </el-table-column>
+          <el-table-column
+            prop="signStatus"
+            label="标记状态"
+            :filters="[{text: '已标记', value: '已标记'},{text: '', value: ''}]"
+            :filter-method="filterHandler"
+            width="100">
+          </el-table-column>
+          <el-table-column label="标记操作" width="150">
+            <template slot-scope="scope">
+              <el-button
+                size="mini"
+                @click="signMatch(scope.$index, scope.row)">标记</el-button>
+              <el-button
+                size="mini"
+                type="danger"
+                @click="cancelSignMatch(scope.$index, scope.row)">删除</el-button>
+            </template>
           </el-table-column> 
           <el-table-column
             prop="bifen"
             label="比分"
             width="70">
+          </el-table-column>
+          <el-table-column
+            prop="preResult"
+            label="预测结果"
+            width="100">
           </el-table-column>
           <!-- <el-table-column
             prop="smax"
@@ -182,11 +219,11 @@
 </el-card>
     
       
-  </div>
+   </el-col>
 </template>
 
 <script>
-import {getPredictions,upload1DataApi,upload3DataApi}  from '../api/api' 
+import {getPredictions,getNeedAnaNum,upload1DataApi,upload3DataApi}  from '../api/api' 
 
 export default {
   name: 'HelloWorld',
@@ -199,7 +236,9 @@ export default {
        upload1Datapath:'',
        upload3Datapath:'',
        loading:'',
-       conflictVal:0.6
+       conflictVal:0.6,
+       anaFinished:false,
+       getFinished:false
     }
   },
   methods:{
@@ -218,10 +257,12 @@ export default {
             this.$message.error('查询失败');
           }          
           let list = data.data
-          let arr = []
-          debugger
+          let arr = []      
+          // 从localStorage中获取已经标记的id
+          let signIdArr = localStorage.getItem('signIds')?JSON.parse(localStorage.getItem('signIds')):[]
           for(let i of list){
             let o = {}
+            o['id'] = i[0]
             o['type']= i[1]
             o['changci'] = i[2]
             o['docTime'] = i[3]
@@ -234,15 +275,128 @@ export default {
             let valArr = i.slice(9,14)
             o['maxVal'] = Math.max(...valArr)
             let maxIndex = valArr.indexOf( o['maxVal'])
+            o['maxIndex']= maxIndex
             o['maxName'] = this.getMaxName(maxIndex)
             o['isWorth'] = this.checkWorth(maxIndex,o['maxVal'],i.slice(16,19))
-            o['bifen'] = i[14]?i[14]+':'+i[15]:''
+            o['bifen'] = i[14]!=null?i[14]+':'+i[15]:''
+            o['zbf'] = i[14]
+            o['kbf'] = i[15]
+            o['signStatus'] = signIdArr.indexOf(i[0])>-1?'已标记':''
+            // 标记之后再出结果
+            if(signIdArr.indexOf(i[0])>-1 && i[14]!=null && i[15]!=null){
+              o['preResult'] = this.getPreResult(maxIndex,i[14],i[15],i[7])
+            }else{
+              o['preResult']= ''
+            }
             //o['isConflict']=this.checkIsConflict(i[9],i[11],i[13],i[15],i[17],i[8]||i[7])
             arr.push(o)
           }
           this.tableData = arr
         })
 
+    },
+    getPreResult(maxIndex,zbf,kbf,lpk){
+      if (maxIndex in [0,1,2]){           
+        if((maxIndex == 0 && zbf>kbf) || (maxIndex == 1 && zbf==kbf) || (maxIndex == 2 && zbf<kbf)){
+          return '红'
+        }
+        else{
+          return '黑'
+        }                 
+      }
+      else if (zbf-kbf-lpk==0){
+        return '走'
+      }
+      else if(lpk==0){
+        return '走'
+      }      
+      else if(maxIndex==3){
+        let cs = lpk> 0?1:-1
+        if ((zbf-kbf-lpk)*cs == 0.25){
+          return '红半'
+        }else if((zbf-kbf-lpk)*cs>0){
+          return '红'
+        }else if((zbf-kbf-lpk)*cs == -0.25){
+          return '黑半'
+        }else{
+          return '黑'
+        }
+      }
+      else if(maxIndex==4){
+        let cs = lpk> 0?1:-1
+        if ((zbf-kbf-lpk)*cs == -0.25){
+          return '红半'
+        }else if((zbf-kbf-lpk)*cs<0){
+          return '红'
+        }else if((zbf-kbf-lpk)*cs == 0.25){
+          return '黑半'
+        }else{
+          return '黑'
+        }
+      }
+    },
+    signMatch(index,row){
+      debugger
+      let signIdArr = localStorage.getItem('signIds')?JSON.parse(localStorage.getItem('signIds')):[]
+      let set = new Set(signIdArr)
+      set.add(row.id)
+      signIdArr = Array.from(set)
+      localStorage.setItem('signIds',JSON.stringify(signIdArr))
+      let o = row
+      o['signStatus'] = '已标记'
+      if(o['zbf']!=null && o['kbf']!=null){
+        o['preResult'] = this.getPreResult(o['maxIndex'],o['zbf'],o['kbf'],o['pankou'])
+      }else{
+        o['preResult']= ''
+      }
+      this.tableData[index] = o
+
+    },
+    cancelSignMatch(index,row){
+      let o = row
+      let signIdArr = localStorage.getItem('signIds')?JSON.parse(localStorage.getItem('signIds')):[]
+      let set = new Set(signIdArr)
+      if(!set.has(o.id)){
+        return 
+      }
+      set.delete(o.id)
+      signIdArr = Array.from(set)
+      localStorage.setItem('signIds',JSON.stringify(signIdArr))
+      o['signStatus'] = ''
+      o['preResult'] = ''
+      this.tableData[index] = o
+    },
+    filterHandler(value, row, column) {
+      const property = column['property'];
+      return row[property] === value;
+    },
+    tableRowClassName({row, rowIndex}) {
+      debugger
+      if (row['signStatus'] === '已标记') {
+        if(row['preResult'].indexOf('红')>-1){
+          return 'error-row'
+        }else if(row['preResult'].indexOf('黑')>-1){
+          return 'success-row'
+        }
+        return 'warning-row';
+      }
+      
+      return '';
+    },
+    getNeednum(){
+      getNeedAnaNum().then(res=>{
+        let data = res.data
+        if (!res.data || !res.data.success){
+          this.$message.error('查询失败');
+        } 
+        let num = data.data
+        if(num>0){
+          this.anaFinished = false
+        }else{
+          this.anaFinished = true
+        }
+        this.getFinished = true
+      })
     },
     checkWorth(index,val,pArr){
       if(index>2){
@@ -251,10 +405,6 @@ export default {
         return false
       }
       return true
-
-
-
-
     },
     getMaxName(index){
       switch(index){
@@ -294,8 +444,6 @@ export default {
       return false
 
     },
-
-
     setCellClass({row, column, rowIndex, columnIndex}){
       if(columnIndex==8){       
         return this.getClassName(row.smax)
@@ -362,6 +510,7 @@ export default {
     let endTime = startTime + 24*60*60*1000
     this.times = [startTime,endTime]
     this.initPredictions()
+    this.getNeednum()
     this.upload1Datapath = upload1DataApi
     this.upload3Datapath = upload3DataApi
   }
